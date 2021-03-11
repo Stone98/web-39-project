e8107764f66c712ac414c308622f24e24d9bb34b
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-03-11 10:48:08 -0700

    wip

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 require("dotenv").config();
 
-console.log("Web 39 rulez big time");
-
 const express = require("express");
 
 const server = express();
+
+server.use(express.json());
 
 console.log(process.env.NODE_ENV);
 
 // on Heroku, an env variable is called "NODE_ENV" -> "production"
 if (process.env.NODE_ENV === "development") {
   const cors = require("cors");
-  server.use(express.json());
+  server.use(cors());
 }
+
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
