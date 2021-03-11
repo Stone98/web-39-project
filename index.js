84f4f4be167b07ff38818bacb9a0029588bae021
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-03-11 10:43:35 -0700

    wip

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+require("dotenv").config();
+
 console.log("Web 39 rulez big time");
+
+const express = require("express");
+
+const server = express();
+
+console.log(process.env.NODE_ENV);
+
+// on Heroku, an env variable is called "NODE_ENV" -> "production"
+if (process.env.NODE_ENV === "development") {
+  const cors = require("cors");
+  server.use(express.json());
+}
